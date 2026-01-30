6897b9d063d5749c26c2cbcc27eaefcd9b46403a
Marc Culler <culler@users.noreply.github.com>
2026-01-30 12:31:45 -0600

    Return the name as given.

--- tk/macosx/tkMacOSXSend.c
+++ tk/macosx/tkMacOSXSend.c
@@ -691,13 +691,25 @@ RegAddName(
  *	will normally be the same as name, but if name was already in use for
  *	an application then a name of the form "name #2" will be chosen, with
  *	a high enough number to make the name unique.
+ *      
+ *      An crucial exception to the behavior described above arises when Tk is
+ *      being run on a Continuous Integration runner.  The file-based App
+ *      registry which is used to ensure uniqueness cannot be used on CI
+ *      runners because macOS will post a system privacy dialog requesing
+ *      permission to write files.  This causes Wish to hang, since it is not
+ *      possible to dismiss the dialog without user interaction.  We use the
+ *      environment variable CI to determine whether we are being run on a CI
+ *      runner.  If so, this command always returns the name provided as an
+ *      argument.  Note that this makes the command winfo interps ignore all
+ *      other interpreteters.
  *
  * Side effects:
- *	Registration info is saved, thereby allowing the "send" command to be
- *	used later to invoke commands in the application. In addition, the
- *	"send" command is created in the application's interpreter. The
- *	registration will be removed automatically if the interpreter is
- *	deleted or the "send" command is removed.
+ *	In normal usage (i.e. being run interactively) the app name is saved
+ *	in a registry file, thereby allowing the "send" command to be used
+ *	later to invoke commands in the application. In addition, the "send"
+ *	command is created in the application's interpreter. The registration
+ *	will be removed automatically if the interpreter is deleted or the
+ *	"send" command is removed.
  *
  *----------------------------------------------------------------------
  */
@@ -711,16 +723,8 @@ Tk_SetAppName(
 				 * interpreter in later "send" commands. Must
 				 * be globally unique. */
 {
-    /*
-     * The App registry cannot be used on CI runners because macOS will post
-     * a dialog asking for permission to write files.  This causes tktest to
-     * hang, since it is not possible to dismiss the dialog.  We check the
-     * environment variable CI to determine whether we are being run on a CI
-     * runner.
-     */
-
     if (getenv("CI")) {
-	return "Wish";
+	return name;
     }
     RegisteredInterp *riPtr;
     TkWindow *winPtr = (TkWindow *) tkwin;
